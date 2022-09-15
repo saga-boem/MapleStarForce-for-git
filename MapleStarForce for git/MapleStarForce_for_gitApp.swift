691a21b86a5f7fb98f564cd5a1459d60ddce3511
Seo Hyoboem <22931005@edu.cc.saga-u.ac.jp>
2022-09-15 23:55:25 +0900

    Initial Commit

---
+++ MapleStarForce for git/MapleStarForce_for_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapleStarForce_for_gitApp.swift
+//  MapleStarForce for git
+//
+//  Created by hyobeom seo on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct MapleStarForce_for_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
